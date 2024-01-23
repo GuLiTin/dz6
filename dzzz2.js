7b009f7925fac14a8634265997654c90156a0b84
GuLiTin <116506471+GuLiTin@users.noreply.github.com>
2024-01-23 19:04:02 +0300

    Add files via upload

---
+++ dzzz2.js
@@ -0,0 +1,11 @@
+function Random (){ 
+    const max = 100;
+    return Math.round(Math.random() * max );
+   }
+   
+   const btn = document.getElementById('button');
+   const div = document.getElementById('div');
+   
+   btn.onclick = function() {
+     div.innerHTML = Random();
+   }
